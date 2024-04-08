45fd6ae450e6d6956880a8f9dc3a4c1d02ec2ead
Foffa <schmuck.christopher@gmail.com>
2024-04-08 09:25:17 +0200

    Add color selection

--- app/devices/[id]/page.tsx
+++ app/devices/[id]/page.tsx
@@ -3,6 +3,26 @@ import "@styles/globals.css";
 import Image from "next/image";
 import { Range, getTrackBackground } from 'react-range';
 import { useState } from "react";
+import Wheel from '@uiw/react-color-wheel';
+import { hsvaToHex, getContrastingColor } from '@uiw/color-convert';
+import Swatch from '@uiw/react-color-swatch';
+import ReactGPicker from 'react-gcolor-picker';
+import clsx from 'clsx';
+
+function Point(props: { color?: string; checked?: boolean }) {
+    if (!props.checked) return null;
+    return (
+      <div
+        style={{
+          height: 5,
+          width: 5,
+          borderRadius: '50%',
+          backgroundColor: getContrastingColor(props.color!),
+        }}
+      />
+    );
+  }
+  
 
 export default function DeviceControl({ 
     params
@@ -10,10 +30,12 @@ export default function DeviceControl({
     params: {id: string}
 }) {
     const [value, setValue] = useState([60]);
+    const [mode, setMode] = useState<number>(1);
+    const [hsva, setHsva] = useState({ h: 214, s: 43, v: 90, a: 1 });
 
     return (
         <div className="device-bg py-24 px-10 lg:px-80 md:px-40 sm:px-12 min-h-dvh">
-        <div className="bg-gray-800/50 rounded-lg py-20 px-10 min-w-96 text-slate-50">
+        <div className="bg-gray-800/50 rounded-xl py-20 px-10 min-w-96 text-slate-50">
             <div className="flex flex-row gap-3">
                 <Image src="/assets/icons/bulb.svg" width={50} height={50} alt="bulb" />
                 <div>
@@ -26,8 +48,8 @@ export default function DeviceControl({
             </div>
             <div className="my-10">
                 <div>
-                    <p className="font-semibold text-xl text-slate-200">Brightness</p>
-                    <p>{value}</p>
+                        <p className="font-semibold text-xl text-slate-200 inline-block">Brightness</p>
+                        <p className="inline-block float-right">{value}%</p>
                     <div className="py-10 max-w-2xl flex items-center gap-5">
                         <Image width={20} height={20} src="/assets/icons/brightness.svg" alt="light dimm"/>
                         <Range 
@@ -67,7 +89,44 @@ export default function DeviceControl({
                     </div>
                 </div>
                 <div>
-                    <p>Mode pills</p>
+                    <p className="font-semibold text-xl text-slate-200">Mode</p>
+                    <div className="mt-4">
+                        <ul className="list-none h-16">
+                            <li className={clsx("rounded-full bg-slate-900 float-left px-5 py-2 mx-1 cursor-pointer", { "bg-slate-500": mode === 0 })} onClick={() => {setMode(0)}}>Color</li>
+                            <li className={clsx("rounded-full bg-slate-900 float-left px-5 py-2 mx-1 cursor-pointer", { "bg-slate-500": mode === 1 })} onClick={() => {setMode(1)}}>Gradient</li>
+                            <li className={clsx("rounded-full bg-slate-900 float-left px-5 py-2 mx-1 cursor-pointer", { "bg-slate-500": mode === 2 })} onClick={() => {setMode(2)}}>Effect</li>
+                        </ul>
+                        {mode === 0 ? (
+                            <div className="flex flex-col items-center">
+                                <Wheel width={200} height={200} color={hsva} onChange={(color) => setHsva({ ...hsva, ...color.hsva })} />
+                                <Swatch
+                                    colors={[ '#F44E3B', '#FE9200', '#FCDC00', '#DBDF00' ]}
+                                    color={hsvaToHex(hsva)}
+                                    rectProps={{
+                                        children: <Point />,
+                                        style: {
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        },
+                                    }}
+                                    onChange={(hsvColor) => {
+                                        setHsva(hsvColor)
+                                    }}
+                                    />
+
+                                <div style={{ width: '100%', height: 34, marginTop: 20, background: hsvaToHex(hsva) }}></div>
+                            </div>
+                        ) : mode === 1 ? (
+                            <div>
+                                <ReactGPicker value='linear-gradient(90deg, rgb(251, 171, 126) 8.00%, rgb(247, 206, 104) 92.00%)' gradient showGradientAngle={true} showGradientPosition={false} showGradientMode={false} solid={false} showAlpha={false} defaultColors={['linear-gradient(270deg, rgb(251, 171, 126) 8.00%, rgb(247, 206, 104) 92.00%)',]} onChange={() => {}} />;
+                            </div>
+                        ) : (
+                            <div>
+                                test3
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
