081fda08e168c880cb57a44e3cfe09771a3d3480
Foffa <schmuck.christopher@gmail.com>
2024-04-11 21:36:04 +0200

    Dynamic links

--- components/Devices.tsx
+++ components/Devices.tsx
@@ -2,22 +2,18 @@ import Link from "next/link";
 import Card from "./card";
 
 const Devices = () => {
+    const devices = [{name: "Led Strip", type: 0}, {name: "LED Strip Monitor", type: 0}, {name: "LED Bar", type: 1}, {name: "LED Matrix", type: 2}]
     return (
         <div className="text-slate-100">
             <h2 className="font-semibold text-3xl">Devices</h2>
             <p className="text-slate-500">Below are all smarthome devices found in your local network</p>
             <div className="w-50 py-10 flex flex-row flex-wrap justify-center gap-4">
-                <Link href="/devices/1">
-                    <Card name="Led Strip Bett" type={0} />
-                </Link>
-                <Card name="Led Strip Monitor" type={0} />
-                <Card name="Led Matrix" type={2} />
-                <Card name="Led Bar" type={1} />
-                <Card name="Led Strip Bett" type={1}/>
-                <Card name="Led Strip Bett" type={1}/>
-                <Card name="Led Strip Bett" type={1}/>
-                <Card name="Led Strip Bett" type={1}/>
-                <Card name="Led Strip Bett" type={1}/>
+                { devices.map((device, index) => (
+                    <Link href={"/devices/" + index}>
+                    <Card name={device.name} type={device.type}/>
+                    </Link>
+                ))
+                }
             </div>
         </div>
     )
