f1af8196571cb2730f8734446de47b8c9e08ad54
Foffa <schmuck.christopher@gmail.com>
2024-09-01 14:05:21 +0200

    delete old db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import Devices from "@components/Devices";
 import Scenes from "@components/Scenes"
-import { checkDevices } from "@db/data";
 
 export default function Home() {
-  checkDevices();
   return (
     <div className="py-24 px-10 max-w-3xl">
       <div className="main"></div>
